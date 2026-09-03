042368740e751ca5f7990859efe9e4f78eaa0bb4
L Lawliet <68460205+L-Lawlite@users.noreply.github.com>
2026-09-03 18:56:03 +0530

    Custom Keybind

---
+++ src/main/java/keybinding/TestKeyMapping.java
@@ -0,0 +1,46 @@
+package keybinding;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.lawliet.testmod.TestMod;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.common.util.Lazy;
+import org.lwjgl.glfw.GLFW;
+
+@EventBusSubscriber(modid = TestMod.MODID, value =  Dist.CLIENT)
+public class TestKeyMapping {
+    public static final KeyMappingRegister TEST = KeyMappingRegister.create(new KeyMapping(getKeyTranslate("test"), InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_K, KeyMapping.Category.MISC));
+
+
+    private static String getKeyTranslate(String name) {
+        return "key.%s.%s".formatted(TestMod.MODID, name);
+    }
+
+    public record KeyMappingRegister(KeyMapping keyMapping, Lazy<KeyMapping> keyPress) {
+
+        public static KeyMappingRegister create(KeyMapping keyMapping) {
+            return new KeyMappingRegister(keyMapping, Lazy.of(() -> keyMapping));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent.Post event) {
+        LocalPlayer player = Minecraft.getInstance().player;
+        assert player != null;
+        while(TEST.keyPress().get().consumeClick()) {
+            player.sendSystemMessage(Component.literal("Test Key pressed"));
+        }
+    }
+
+    @SubscribeEvent
+    public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
+        event.register(TEST.keyPress().get());
+    }
+}
